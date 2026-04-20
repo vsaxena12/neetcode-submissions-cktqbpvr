126cb29c069243bfd657a5134f9d03cf5373bbbf
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2026-04-20 16:06:09 -0500

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+
+        int j = 0;
+        
+        for(int i=0; i<prices.length; i++) {
+            while(prices[i] - prices[j] < 0) {
+                j++;
+            }
+            max = Math.max(max, prices[i] - prices[j]);
+        }
+        return max;
+    }
+}
