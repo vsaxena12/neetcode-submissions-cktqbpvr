e3c077fcce228cf7bddccf2765d9dba3ef7a6bc2
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2026-04-25 12:45:34 -0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(int i=0; i<strs.length; i++) {
+            String hash = hashcode(strs[i]);
+            map.putIfAbsent(hash, new ArrayList<>());
+            map.get(hash).add(strs[i]);
+        }    
+        return new ArrayList<>(map.values());
+    }
+
+    private String hashcode(String s) {
+        int[] result = new int[26];
+
+        for(char c: s.toCharArray()) {
+            result[c-'a']++;
+        }
+        return Arrays.toString(result);
+     }
+}
