af95508196a6b3e63ed82a03759e59392cfca5b3
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2026-04-24 20:42:34 -0500

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        for(char c: t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)-1);
+        }
+
+        for(Map.Entry<Character, Integer> entry: map.entrySet()) {
+            if(entry.getValue() != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
