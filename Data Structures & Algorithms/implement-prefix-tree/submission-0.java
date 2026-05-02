2015a84df2f62e2621a15f094506766256869e28
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2026-05-02 08:37:16 -0500

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,45 @@
+class TrieNode {
+    boolean word;   // isLeaf
+    Map<Character, TrieNode> children = new HashMap<>();
+}
+
+class PrefixTree {
+    TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();     
+    }
+
+    public void insert(String word) {
+        TrieNode curr = this.root;
+        for (char c : word.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.word = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = this.root;
+        for (char c : word.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return curr.word;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = this.root;
+        for (char c : prefix.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return true;
+    }
+}
