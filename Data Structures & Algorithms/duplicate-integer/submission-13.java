f325d1e7bf21d9effc59c3ea108816ed93f331be
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2026-05-03 18:23:27 -0500

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // [1,2,3,4] -> sorting or hashset
+
+        // sorting -> nums[i] == nums[i-1] return true else false
+        // Arrays.sort(nums);
+        // for(int i=1; i<nums.length; i++) {
+        //     if(nums[i] == nums[i-1]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // hashset -> nums[i] if contains nums[i] return true else false
+        Set<Integer> set = new HashSet<>(); 
+        for(int n: nums) {
+            if(set.contains(n)) {
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
