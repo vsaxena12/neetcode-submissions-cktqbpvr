afcad440e9c946438bda48a36a6766f4d98ba64c
Varun Saxena <36344027+vsaxena12@users.noreply.github.com>
2026-05-08 16:50:15 -0500

    Add: 4sum - submission-3

---
+++ Data Structures & Algorithms/4sum/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                int l = nums.length-1;
+                int k=j+1;
+                while(k<l) {
+                    int sum = nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        list.add(nums[l]);
+                        set.add(list);
+                        k++;
+                        l--;
+                    } else if(sum > target) {
+                        l--;
+                    } else {
+                        k++;
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
